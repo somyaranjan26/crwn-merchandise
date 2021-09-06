225377a239b0711ccc7588530c294da1de0304ff
Somyaranjan Rout <somyaranjanmac1@gmail.com>
2021-09-06 17:44:58 +0530

    created shop page

---
+++ src/components/collection-item/collection-item.component.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+import './collection-item.style.scss';
+
+const CollectionItem = ({name, price, imageUrl}) => (
+    <div className="collection-item">
+        <div 
+            className="image"
+            style={{
+                backgroundImage: `url(${imageUrl})`
+            }}
+        />
+        <div className="collection-footer">
+            <div className="name">{name}</div>
+            <div className="price">{price}</div>
+        </div>
+    </div>
+)
+
+export default CollectionItem;
